e7fd3c0c52ffb8f921dd282f7e70e2e1b3ceb306
Jessica <andradejsscl@gmail.com>
2022-10-22 10:56:58 -0400

    commit tree ff29ca792aac7dd87b310f0bc61c3ebd84cdac93

---
+++ index.js
@@ -0,0 +1,11 @@
+function introduction(name) {
+    return `Hi, my name is ${name}.`
+}
+function introduction (name1, name2) {
+    let name1 = "Aki";
+    name2 = "Samip";
+    console.log(`The two names are ${name1} and ${name2}.`);
+}
+function introductionWithLanguage (name, language) {
+    return "Hi, my name is ${name} and I am learning to program in ${language}."
+}
